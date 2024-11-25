b4fb06c416d4ba94a63b1c0c9b46ef62b19162b5
Usama Sabir <usamasabir.com.pk@gmail.com>
2024-11-26 01:22:26 +0500

    changes

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export function ToDoList() {
-  let [todos, setTodos] = useState([{ task: "Sample Task", id: uuidv4 }]);
+  let [todos, setTodos] = useState([
+    { task: "Sample Task", id: uuidv4(), isDone: false },
+  ]);
   let [newTodos, setNewTodos] = useState("");
 
   let addNewTask = () => {
     setTodos((prevTodos) => {
-      return [...prevTodos, { task: newTodos, id: uuidv4() }];
+      return [...prevTodos, { task: newTodos, id: uuidv4(), isDone: false }];
     });
     setNewTodos("");
   };
@@ -45,6 +47,33 @@ export function ToDoList() {
       })
     );
   };
+
+  let markasDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  let deleteAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      })
+    );
+  };
+
   return (
     <div className="min-h-screen bg-gray-900 text-white p-5">
       <p className="text-2xl font-bold">ToDoList</p>
@@ -71,7 +100,16 @@ export function ToDoList() {
       <ul className="list-disc list-inside mt-2">
         {todos.map((todo) => (
           <li key={todo.id} className="mt-1">
-            <span>{todo.task}</span> &nbsp; &nbsp; &nbsp;
+            <span
+              style={
+                todo.isDone
+                  ? { textDecorationLine: "line-through ", color: "red" }
+                  : {}
+              }
+            >
+              {todo.task}
+            </span>
+            &nbsp; &nbsp; &nbsp;
             <button
               className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
               onClick={() => deleteTodo(todo.id)}
@@ -85,6 +123,13 @@ export function ToDoList() {
             >
               UpperCase One
             </button>
+            &nbsp; &nbsp; &nbsp;
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              onClick={() => markasDone(todo.id)}
+            >
+              Done
+            </button>
           </li>
         ))}
       </ul>
@@ -95,6 +140,13 @@ export function ToDoList() {
         onClick={upperCaseAll}
       >
         Uppercase All
+      </button>
+      &nbsp; &nbsp; &nbsp;
+      <button
+        className="px-4 py-2 bg-red-500 text-white rounded hover:red-purple-600"
+        onClick={deleteAll}
+      >
+        Delete All
       </button>
     </div>
   );
